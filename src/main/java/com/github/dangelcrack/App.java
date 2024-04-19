02074a58335efeec44c08a011a3c028216af2153
Dangelcrack <angelguerrero540@gmail.com>
2024-04-19 23:40:18 +0200

    ConexionMariadb

--- src/main/java/com/github/dangelcrack/App.java
+++ src/main/java/com/github/dangelcrack/App.java
@@ -1,5 +1,8 @@
 package com.github.dangelcrack;
 
+import com.github.dangelcrack.view.AppController;
+import com.github.dangelcrack.view.Scenes;
+import com.github.dangelcrack.view.View;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,23 +16,26 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
+    public static Scene scene;
+    public static Stage stage;
+    public static AppController currentController;
 
+    //este el es primer método que se ejecuta al abrir la primera ventana
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        //view/layout.fxml
+        View view = AppController.loadFXML(Scenes.ROOT);
+        scene = new Scene(view.scene, 640, 480);
+        currentController = (AppController) view.controller;
+        currentController.onOpen(null);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+    public static void setRoot(String fxml) throws IOException {
+        //  scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
 
     public static void main(String[] args) {
         launch();
---
+++ src/main/java/com/github/dangelcrack/view/View.java
@@ -0,0 +1,8 @@
+package com.github.dangelcrack.view;
+
+import javafx.scene.Parent;
+
+public class View {
+    public Parent scene;
+    public Controller controller;
+}
